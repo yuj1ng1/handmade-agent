126a276f279f770497e1ff6985d5b17254e083c5
yuj1ng1 <2638058297@qq.com>
2026-09-21 09:57:19 +0800

    Configure local LangChain embedding model

--- langchain_rag_demo.py
+++ langchain_rag_demo.py
@@ -1,5 +1,8 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 
 embeddings = HuggingFaceEmbeddings(
-    model_name=""
+    model_name="BAAI/bge-small-zh-v1.5",
+    model_kwargs={
+        "local_files_only":True
+    }
 )
