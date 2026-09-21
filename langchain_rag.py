b5fa331fb9f8830b1920e65c6f506dd2bf7c9aa8
yuj1ng1 <2638058297@qq.com>
2026-09-21 19:42:17 +0800

    更改了一些代码错误

--- langchain_rag.py
+++ langchain_rag.py
@@ -44,22 +44,16 @@ class LangChainRag:
         self.chain=None
 
     def load(self):
+        if self.vector_store is not None:
+            return 
         self.embeddings=HuggingFaceEmbeddings(
             model_name=self.model_name,
             model_kwargs={
                 "local_files_only":True
             }
         )
-        loader=Docx2txtLoader(self.file_path)
-        documents=loader.load()
-
-        text_splitter=RecursiveCharacterTextSplitter(
-            chunk_size=self.chunk_size,
-            chunk_overlap=self.overlap
-        )
-        chunks=text_splitter.split_documents(documents)
+        current_hash=self.get_cache_hash()
         cache_valid=False
-        current_hash=self.get_chunks_hash(chunks)
 
         if self.vector_path.exists() and self.hash_path.exists():
             old_hash=self.hash_path.read_text(
@@ -73,22 +67,32 @@ class LangChainRag:
                 self.embeddings,
                 allow_dangerous_deserialization=True
             )
-        else:
-            self.vector_store=FAISS.from_documents(
-                chunks,
-                self.embeddings
-            )
+            return
+        
+        loader=Docx2txtLoader(self.file_path)
+        documents=loader.load()
+        text_splitter=RecursiveCharacterTextSplitter(
+            chunk_size=self.chunk_size,
+            chunk_overlap=self.overlap
+        )
+        chunks=text_splitter.split_documents(documents)
 
-            self.vector_store.save_local(str(self.vector_path))
-            self.hash_path.write_text(
-                current_hash,
-                encoding="utf-8"
-            )
+        self.vector_store=FAISS.from_documents(
+            chunks,
+            self.embeddings
+        )
+        self.vector_store.save_local(
+            str(self.vector_path)
+        )
+        self.hash_path.write_text(
+            current_hash,
+            encoding="utf-8"
+        )
 
     def build_chain(self):
         if self.retriever is None:
             self.build_retriever()
-            
+
         prompt_template = PromptTemplate.from_template(
         """
         请只根据下面提供的资料回答问题。
@@ -112,7 +116,7 @@ class LangChainRag:
         self.chain=(
             {
                 "context":
-                    self.retriever|RunnableLambda(self.format_document),
+                    self.retriever|RunnableLambda(self.format_documents),
                 "question":RunnablePassthrough()
             }
             |prompt_template
@@ -140,23 +144,22 @@ class LangChainRag:
             self.build_chain()
         return self.chain.invoke(question)
     
-    def get_chunks_hash(self,chunks):
-        texts=[]
-        for chunk in chunks:
-            texts.append(
-                chunk.page_content #langchain中的chunks里面是一个一个document
-            )
-        full_text="\n".join(texts)
-        cache_text=(
-            full_text
+    def get_cache_hash(self):
+        file_bytes=self.file_path.read_bytes()
+
+        config_text=(
+            self.model_name
             +"\n"
-            +self.model_name
+            +str(self.chunk_size)
+            +"\n"
+            +str(self.overlap)
         )
-        return hashlib.md5(
-            cache_text.encode("utf-8")
-        ).hexdigest()
+        hasher=hashlib.md5()
+        hasher.update(file_bytes.encode("utf-8"))
+        hasher.update(config_text)
+        return hasher.hexdigest()
     
-    def format_document(self,documents):
+    def format_documents(self,documents):
         context_parts=[]
         for document in documents:
             context_parts.append(
